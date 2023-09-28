6689aec47867460225e5e6fa8823f9206cf34afa
Paul Davis <paul.davis438@gmail.com>
2023-09-28 18:25:29 +0100

    Personalized the landing page and got rid of some default project code and images.

--- pebscicle/src/App.js
+++ pebscicle/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+//Import Images
+import smiley from './img/Smiley.jpeg'
+//Import style sheets
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={smiley} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Paul K. Davis - Portfolio Website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
