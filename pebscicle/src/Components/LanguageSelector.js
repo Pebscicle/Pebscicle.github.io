b5c532645ecdeb8914bb4a1c345d030cb700ef68
Paul Davis <paul.davis438@gmail.com>
2023-10-12 20:34:50 +0100

    More work on navbar

---
+++ pebscicle/src/Components/LanguageSelector.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import './LanguageSelector.css';
+
+
+function LanguageSelector(props) {
+  return (
+    <div className="language-selector">
+        
+    </div>
+  );
+}
+
+export default LanguageSelector;
--- pebscicle/src/Components/Navbar.js
+++ pebscicle/src/Components/Navbar.js
@@ -3,11 +3,16 @@ import React from 'react';
 
 import './Navbar.css';
 
+import '../Components/LanguageSelector'
+import LanguageSelector from '../Components/LanguageSelector';
+
 
 function Navbar(props) {
   return (
     <nav className='navbar'>
         <p>content</p>
+
+        <LanguageSelector></LanguageSelector>
     </nav>
   );
 }
