5466d0d90ed237207857c85004eb49dc245a4d12
Paul Davis <paul.davis438@gmail.com>
2024-07-12 19:26:14 +0200

    Set missingSuspenseWithCSRBailout to false.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    experimental: {
+      missingSuspenseWithCSRBailout: false, // The option you want to add
+    },
   };
   
   module.exports = nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
     reactStrictMode: true,
     distDir: './dist', // Changes the build output directory to `./dist/`.
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    experimental: {
+      missingSuspenseWithCSRBailout: false, // The option you want to add
+    },
   }
    
   export default nextConfig
+
